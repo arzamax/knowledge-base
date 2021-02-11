6ae9d094cdb73a4625739a49d8c9ac083935c368
Maksim Arzamastsev <arzamax@gmail.com>
2021-02-11 20:28:52 +0400

    feat: added optimized quick-sort

--- src/algorithms/sort/quick-sort.ts
+++ src/algorithms/sort/quick-sort.ts
@@ -1,4 +1,4 @@
-const quickSortRecursive = (arr: any[]): any[] => {
+const simpleQuickSort = (arr: any[]): any[] => {
   if (arr.length < 2) return arr;
   const pivotIndex = Math.floor(Math.random() * arr.length);
   const pivot = arr[pivotIndex];
@@ -15,5 +15,38 @@ const quickSortRecursive = (arr: any[]): any[] => {
     }
   }
 
-  return [...quickSortRecursive(left), pivot, ...quickSortRecursive(right)];
+  return [...simpleQuickSort(left), pivot, ...simpleQuickSort(right)];
+};
+
+const partition = (arr: any[], left: number, right: number): number => {
+  const middle = Math.floor((right + left) / 2);
+  const pivot = arr[middle];
+  let i = left;
+  let j = right;
+
+  while (i <= j) {
+    while (arr[i] < pivot) i++;
+    while (arr[j] > pivot) j--;
+
+    if (i <= j) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+      j--;
+    }
+  }
+
+  return i;
+};
+
+const quickSort = (arr: any[], left = 0, right = arr.length - 1): any[] => {
+  const len = arr.length;
+
+  if (len > 1) {
+    const index = partition(arr, left, right);
+
+    if (left < index - 1) quickSort(arr, left, index - 1);
+    if (index < right) quickSort(arr, index, right);
+  }
+
+  return arr;
 };
